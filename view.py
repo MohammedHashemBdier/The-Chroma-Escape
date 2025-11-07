18864a1c6c3f5474d6224fcb340633fa3564f72b
Mohammed Hashem Bdier <mhb20033@gmail.com>
2025-11-08 00:36:21 +0300

    Add initial view setup with color constants and cell dimensions

---
+++ view.py
@@ -0,0 +1,15 @@
+import pygame
+from model import GameState, Block, Board
+
+CELL_SIZE = 80
+GRID_LINE_COLOR = (150, 150, 150)
+BACKGROUND_COLOR = (240, 240, 240) 
+WIN_COLOR = (0, 255, 0)
+
+COLOR_MAP = {
+    "red": (255, 0, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "gray": (100, 100, 100),
+}
+
