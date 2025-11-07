c146fda1c21cb0e1cc3103e95130ad254d940b5a
Mohammed Hashem Bdier <mhb20033@gmail.com>
2025-11-07 23:25:11 +0300

    Refactor Block class to include movement_type parameter in constructor

--- model.py
+++ model.py
@@ -3,3 +3,11 @@ class MovementType(Enum):
     HORIZONTAL = 1
     VERTICAL = 2
     ANY = 3
+
+class Block:
+    def __init__(self, color: str, x: int, y: int, shape: list, movement_type: MovementType):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.movement_type = movement_type
